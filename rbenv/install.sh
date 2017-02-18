e111861b33b26669fe400f62655de02f82e3dba4
seenkoo <shinkupro@gmail.com>
2017-02-18 11:54:26 +0500

    chmod +x all installers

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -1,5 +1,5 @@
 which rbenv >/dev/null 2>&1 || { echo 'rbenv is not installed'; exit 1; }
-list=$(rbenv root)/default-gems
+list=/usr/local/opt/rbenv/default-gems
 test -e $list && exit 0
 echo 'bundler
 rack
