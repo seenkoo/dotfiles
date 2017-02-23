c2dee7271990afd660bb9c17fb2c3347c1b254ab
seenkoo <shinkupro@gmail.com>
2017-02-23 07:33:20 +0500

    feat: spectrum_ls and spectrum_bls now show name and hex code of each xterm color

--- zsh/spectrum.zsh
+++ zsh/spectrum.zsh
@@ -1,9 +1,6 @@
-#! /bin/zsh
 # A script to make using 256 colors in zsh less painful.
-# P.C. Shyamshankar <sykora@lucentbeing.com>
-# Copied from http://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
 
-typeset -AHg FX FG BG
+typeset -AHg FX FG BG XTERM_COLOR_NAMES XTERM_COLOR_HEX_CODES
 
 FX=(
     reset     "%{[00m%}"
@@ -14,24 +11,27 @@ FX=(
     reverse   "%{[07m%}" no-reverse   "%{[27m%}"
 )
 
-for color in {000..255}; do
-    FG[$color]="%{[38;5;${color}m%}"
-    BG[$color]="%{[48;5;${color}m%}"
+cat "$(dirname "$0")/xterm.colors" | while read code hex name ; do
+  FG[$code]="%{[38;5;${code}m%}"
+  BG[$code]="%{[48;5;${code}m%}"
+  XTERM_COLOR_NAMES[$code]="$name"
+  XTERM_COLOR_HEX_CODES[$code]="$hex"
 done
-
-
-ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
 
 # Show all 256 colors with color number
 function spectrum_ls() {
   for code in {000..255}; do
-    print -P -- "$code: %{$FG[$code]%}$ZSH_SPECTRUM_TEXT%{$reset_color%}"
+    local name="$XTERM_COLOR_NAMES[$code]"
+    local hex="$XTERM_COLOR_HEX_CODES[$code]"
+    print -P -- "$code %{$FG[$code]%}$hex%{$reset_color%} $name"
   done
 }
 
 # Show all 256 colors where the background is set to specific color
 function spectrum_bls() {
   for code in {000..255}; do
-    print -P -- "$code: %{$BG[$code]%}$ZSH_SPECTRUM_TEXT%{$reset_color%}"
+    local name="$XTERM_COLOR_NAMES[$code]"
+    local hex="$XTERM_COLOR_HEX_CODES[$code]"
+    print -P -- "$code %{$BG[$code]%}$hex%{$reset_color%} $name"
   done
 }
