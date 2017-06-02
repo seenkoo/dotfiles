5564b974c721bf93fcb4193f81774f0fa7bb4dfd
seenkoo <shinkupro@gmail.com>
2017-06-02 18:09:10 +0500

    add default-gems and extended-mri-gems lists to rbenv

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -1,10 +1,92 @@
 which rbenv >/dev/null 2>&1 || { echo 'rbenv is not installed'; exit 1; }
-list=$(rbenv root)/default-gems
-test -e $list && exit 0
-echo 'bundler
+default_gems_file="$(rbenv root)/default-gems"
+extended_mri_gems_file="$(rbenv root)/extended-mri-gems"
+
+echo \
+'bundler
 rack
 rake
 rdoc
 yard
-rails
-powder' > $list
+rubygems-update
+gem-path
+awesome_print
+rails' > $default_gems_file
+
+echo \
+'addressable
+after_do
+axiom-types
+bacon
+bropages
+chronic
+coderay
+coercible
+coffee-script
+coffee-script-source
+concurrent-ruby
+concurrent-ruby-ext
+connection_pool
+corefines
+database_cleaner
+did_you_mean
+eventmachine
+execjs
+facets
+factory_girl
+factory_girl_rails
+faker
+faraday
+fattr
+gifme
+gist
+git
+github_api
+guard
+hashie
+highline
+http
+i18n
+imgkit
+listen
+mail
+mimemagic
+minitest
+multi_json
+multi_xml
+nokogiri
+oauth2
+oj
+ox
+pry
+pry-byebug
+pry-rails
+pry-coolline
+pry-toys
+puma
+pygments.rb
+qrencoder
+rainbow
+require_all
+rspec
+rspec-rails
+rubocop
+ruby-progressbar
+rubyzip
+safe_yaml
+sass
+sass-rails
+shellany
+specific_install
+streamio-ffmpeg
+sqlite3
+test-unit
+thin
+thor
+typhoeus
+uglifier
+url_regex
+virtus
+webmock
+wkhtmltoimage-binary
+yajl-ruby' > $extended_mri_gems_file
