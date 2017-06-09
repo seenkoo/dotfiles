7ade586259687f1f192d8253a2b724079f3f8ded
seenkoo <shinkupro@gmail.com>
2017-06-09 10:41:19 +0500

    include iterm2_shell_integration by default

--- iterm2.symlink/completion.zsh
+++ iterm2.symlink/completion.zsh
@@ -1,1 +1,1 @@
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+test -e "${HOME}/.iterm2/iterm2_shell_integration.zsh" && source "${HOME}/.iterm2/iterm2_shell_integration.zsh"
