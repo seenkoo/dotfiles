e8bf8013cdc9e90defe1c6d4d6a82f4e6cae7ad4
seenkoo <shinkupro@gmail.com>
2018-05-24 12:52:04 +0500

    update zsh theme prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,78 +1,59 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-git_branch() {
-  git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'}
-}
-
-# Checks if working tree is dirty
-#   https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
-function parse_git_dirty() {
-  local STATUS=''
-  local FLAGS
-  FLAGS=('--porcelain')
-  if [[ "$(command git config --get zsh.hide-dirty)" != "1" ]]; then
-    if [[ $POST_1_7_2_GIT -gt 0 ]]; then
-      FLAGS+='--ignore-submodules=dirty'
-    fi
-    if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
-      FLAGS+='--untracked-files=no'
-    fi
-    STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
-  fi
-  if [[ -n $STATUS ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
-  else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-  fi
-}
-
-git_dirty() {
-  if $(! git status -s &>/dev/null)
-  then
-    echo ""
-  else
-    if [[ $(git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$(git symbolic-ref HEAD 2>/dev/null) || return
- echo "${ref#refs/heads/}"
-}
-
-# This assumes that you always have an origin named `origin`, and that you only
-# care about one specific origin. If this is not the case, you might want to use
-# `git cherry -v @{upstream}` instead.
-need_push () {
-  if [ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]
-  then
-    number=$(git cherry -v origin/$(git symbolic-ref --short HEAD) | wc -l | bc)
-
-    if [[ $number == 0 ]]
-    then
-      echo " "
-    else
-      echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
-    fi
-  fi
-}
-
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
-
-export PROMPT=$'\n$(battery-status)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
-
-precmd() {
-  set_prompt
-}
+# autoload colors && colors
+# # cheers, @ehrenmurdick
+# # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+#
+# git_branch() {
+#   git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'}
+# }
+#
+# # Checks if working tree is dirty
+# #   https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
+# function parse_git_dirty() {
+#   local STATUS=''
+#   local FLAGS
+#   FLAGS=('--porcelain')
+#   if [[ "$(command git config --get zsh.hide-dirty)" != "1" ]]; then
+#     if [[ $POST_1_7_2_GIT -gt 0 ]]; then
+#       FLAGS+='--ignore-submodules=dirty'
+#     fi
+#     if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
+#       FLAGS+='--untracked-files=no'
+#     fi
+#     STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+#   fi
+#   if [[ -n $STATUS ]]; then
+#     echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+#   else
+#     echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+#   fi
+# }
+#
+# # This assumes that you always have an origin named `origin`, and that you only
+# # care about one specific origin. If this is not the case, you might want to use
+# # `git cherry -v @{upstream}` instead.
+# need_push () {
+#   if [ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]
+#   then
+#     number=$(git cherry -v origin/$(git symbolic-ref --short HEAD) | wc -l | bc)
+#
+#     if [[ $number == 0 ]]
+#     then
+#       echo " "
+#     else
+#       echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
+#     fi
+#   fi
+# }
+#
+# directory_name() {
+#   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+# }
+#
+# export PROMPT=$'\n$(battery-status)in $(directory_name) $(need_push)\n› '
+# set_prompt () {
+#   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+# }
+#
+# precmd() {
+#   set_prompt
+# }
--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,37 +1,67 @@
-# ls colors
-autoload -U colors && colors
-
+# Colors
+autoload colors; colors;
 # LS colors, made with http://geoff.greer.fm/lscolors/
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 export LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
-
-function git_prompt_info() {
-  local ref
-  if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
-    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-  fi
-}
-
-function ruby_prompt_info() {
-  if declare -f rbenv_prompt_info >/dev/null 2>&1; then
-    rbenv_prompt_info
-  else
-    echo "system: $(ruby -v | cut -f-2 -d ' ')"
-  fi
-}
-
-PROMPT='%{$fg[$NCOLOR]%}%n%{$reset_color%}@%{$fg[blue]%}%M %{$fg_no_bold[yellow]%}[%4c]%{$reset_color%} %{$fg[red]%}‹$(ruby_prompt_info)› %{$reset_color%}$(git_prompt_info)%{$reset_color%}
-$ '
-
 if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="green"; fi
 
-# git theming
+# Prompt
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg_no_bold[red]%}%B"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%b%{$fg_bold[blue]%})%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 ZSH_THEME_GIT_PROMPT_DIRTY="*"
+
+# show git branch/tag/name-rev
+function git_prompt_info() {
+  [[ $(command git config --get zsh.hide-prompt 2>/dev/null) ]] && return 0;
+  local ref
+  ref=$(command git symbolic-ref HEAD 2> /dev/null) \
+    || ref=$(command git rev-parse --short HEAD 2> /dev/null) \
+    || return 0
+  printf %s \
+    $ZSH_THEME_GIT_PROMPT_PREFIX \
+    ${ref#refs/heads/} \
+    $(parse_git_dirty) \
+    $ZSH_THEME_GIT_PROMPT_SUFFIX
+}
+
+# checks if working tree is dirty
+function parse_git_dirty() {
+  local STATUS=''
+  local FLAGS=('--porcelain')
+  if [[ "$(command git config --get zsh.hide-dirty)" != "1" ]]; then
+    if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
+      FLAGS+='--untracked-files=no'
+    fi
+    STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+  fi
+  if [[ -n $STATUS ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+  else
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+  fi
+}
+
+# show ruby version
+function ruby_prompt_info() {
+  local ver
+  if declare -f rbenv_prompt_info >/dev/null 2>&1; then
+    ver="$(rbenv_prompt_info)"
+  else
+    ver="system: $(ruby -v | cut -f-2 -d ' ')"
+  fi
+  printf %s \
+    $fg_bold[blue] '‹' $reset_color \
+    $fg_bold[red] $ver $reset_color \
+    $fg_bold[blue] '›' $reset_color
+}
+
+PROMPT='%{$fg[$NCOLOR]%}%n%{$reset_color%}@%{$fg[blue]%}%M%{$reset_color%}'\
+' %{$fg_no_bold[yellow]%}[%4c]%{$reset_color%}'\
+' $(ruby_prompt_info)'\
+' $(git_prompt_info)'\
+'
+$ '
 
 if [[ -n $SSH_CONNECTION ]]; then
   PROMPT="%{$fg[yellow]%}[SSH]%{$reset_color%} $PROMPT"
